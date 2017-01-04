6c568e484be410c753ff6b0de60cb686eb6fc6da
Bruno Araujo <araujobma@gmail.com>
2017-01-04 19:29:36 -0200

    Lecture 13 done.

--- examples/Lecture13/beginning-of-lecture-code/part3/app.js
+++ examples/Lecture13/beginning-of-lecture-code/part3/app.js
@@ -3,7 +3,8 @@
 
   angular.module('MsgApp', [])
   .controller('MsgController', MsgController)
-  .filter('loves', LovesFilter);
+  .filter('loves', LovesFilter)
+  .filter('truth', TruthFilter);
 
   MsgController.$inject = ['$scope', 'lovesFilter'];
   function MsgController($scope, lovesFilter) {
@@ -20,6 +21,13 @@
       return msg;
     };
 
+    $scope.sayLovesMessage = function () {
+      var msg = "Yaakov likes to eat healthy snacks at night!";
+      msg = lovesFilter(msg)
+      return msg;
+    };
+
+    
     $scope.feedYaakov = function () {
       $scope.stateOfBeing = "fed";
     };
@@ -32,4 +40,13 @@
       return input;
     }
   }
+
+  function TruthFilter(){
+    return function (input, target, replace){
+      input = input || "";
+      input = input.replace(target, replace);
+      return input;
+    };
+  }
+
 })();
